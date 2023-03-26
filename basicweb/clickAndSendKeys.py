16c253df3cdf3e7b041fae5ae4c4a683866fd397
zozol <z.ozola.ozolina@gmail.com>
2023-03-26 15:32:24 +0300

    sync test

---
+++ basicweb/clickAndSendKeys.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class ClickAndSendKeys():
+
+    def test(self):
+        baseUrl = 'https://courses.letskodeit.com/practice'
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(10)
+
+        loginLink = driver.find_element(By.XPATH, "//a[contains(text(),'Sign In')]")
+        loginLink.click()
+
+        emailField = driver.find_element(By.ID, "email")
+        emailField.send_keys("test")
+
+        passwordField = driver.find_element(By.ID, "password")
+        passwordField.send_keys("test")
+
+        time.sleep(3)
+
+        emailField.clear()
+
+        time.sleep(3)
+
+        emailField.send_keys("test")
+
+
+
+ff = ClickAndSendKeys()
+ff.test()
